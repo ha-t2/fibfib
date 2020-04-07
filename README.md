3f96609c6b093dd805c60abd37610aefca299aa0
hayama.asai <hayama.asai@geniee.co.jp>
2020-04-07 19:25:26 +0900

    feat: imple yaba code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 dog is print file contents like `cat`
+
+# usage
+```shell script
+go build
+./dog simple.txt
+time ./dog 500000.txt > /dev/null
+```
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
+
+func main() {
+	flag.Parse()
+	fileName := flag.Args()[0]
+	contents := readFile(fileName)
+	fmt.Println(contents)
+}
+
+func readFile(fileName string) string {
+	fp, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	result := ""
+	buf := make([]byte, 1)
+	for {
+		n, err := fp.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		result += string(buf)
+	}
+
+	return result
 }
